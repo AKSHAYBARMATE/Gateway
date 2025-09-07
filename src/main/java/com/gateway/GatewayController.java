4a913367baaac88195610cb5ec06f441b2aeafa3
varad.mayee <varad7798mayee>
2025-09-07 13:35:42 +0530

    Intial Commit

---
+++ src/main/java/com/gateway/GatewayController.java
@@ -0,0 +1,15 @@
+package com.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/gateway")
+public class GatewayController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Gateway is running!";
+    }
+}
---
+++ src/main/java/com/gateway/SwaggerAggregationConfig.java
@@ -0,0 +1,19 @@
+package com.gateway;
+
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//public class SwaggerAggregationConfig {
+//
+//    @Bean
+//    public SwaggerUiConfigParameters swaggerUiConfigParameters(SwaggerUiConfigParameters swaggerUiConfigParameters) {
+//        // Register each microservice Swagger JSON with Gateway Swagger UI
+//
+//        swaggerUiConfigParameters.addUrl("/api/students/v3/api-docs");
+//        swaggerUiConfigParameters.addUrl("/api/v1/common-master/v3/api-docs");
+//
+//        return swaggerUiConfigParameters;
+//    }
+//}
