eea9b2e86bc74b8860d001ae9792f6dcc2693ebd
AKSHAYBARMATE <100841949+AKSHAYBARMATE@users.noreply.github.com>
2025-10-07 00:02:01 +0530

    Update CorsGlobalConfig.java

--- src/main/java/com/gateway/config/CorsGlobalConfig.java
+++ src/main/java/com/gateway/config/CorsGlobalConfig.java
@@ -3,25 +3,24 @@ package com.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
 
 import java.util.Arrays;
 
 @Configuration
-public class CorsGlobalConfig {
+public class GatewayCorsConfig {
+
     @Bean
-    public CorsWebFilter corsWebFilter() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:92", "http://100.96.183.108"));
+        config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+        config.addAllowedHeader("*");
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost")); // React app
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-
-        return new CorsWebFilter(source);
+        return source;
     }
-
 }
